786fb09270dafa4bccde394b6e696af1e30dadd7
stanleyyg11 <stanleyyong8888@gmail.com>
2026-04-29 22:34:45 +0700

    added laplacian method and delegate methods to functions

--- src/main.py
+++ src/main.py
@@ -2,33 +2,54 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Input image
-img = "../asset/rocky.jpeg"
+# img = "../asset/rocky.jpeg"
 
 # Process image to detect edges using various tools from OpenCV
-image_ori = cv2.imread(img, cv2.IMREAD_COLOR)
-image_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+def convert_to_gray(ori_image):
+    image_ori = cv2.imread(ori_image, cv2.IMREAD_COLOR)
+    image_gray = cv2.imread(ori_image, cv2.IMREAD_GRAYSCALE)
+    return image_ori, image_gray
 
-sobelx = cv2.Sobel(image_gray, cv2.CV_64F, 1, 0, ksize=5)
-sobely = cv2.Sobel(image_gray, cv2.CV_64F, 0, 1, ksize=5)
+def sobel_method(image_gray):
+    sobelx = cv2.Sobel(image_gray, cv2.CV_64F, 1, 0, ksize=5)
+    sobely = cv2.Sobel(image_gray, cv2.CV_64F, 0, 1, ksize=5)
 
-gradient_magnitude = cv2.magnitude(sobelx, sobely)
-gradient_magnitude = cv2.convertScaleAbs(gradient_magnitude)
+    gradient_magnitude = cv2.magnitude(sobelx, sobely)
+    result_sobel = cv2.convertScaleAbs(gradient_magnitude)
 
-cv2.imwrite("../asset/rocky_sobel.jpeg", gradient_magnitude)
+    return result_sobel
 
-cv2.imshow("image", gradient_magnitude)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def laplacian_method(image_gray):
+    laplacian = cv2.Laplacian(image_gray, cv2.CV_64F)
+    laplacian_abs = cv2.convertScaleAbs(laplacian)
 
-rgb_img = cv2.cvtColor(image_ori, cv2.COLOR_BGR2RGB)
-sobel_img = cv2.cvtColor(gradient_magnitude, cv2.COLOR_BGR2RGB)
+    return laplacian_abs
 
-plt.subplot(1,2,1)
-plt.title("Original")
-plt.imshow(rgb_img)
+def main(method, img_path):
+    image_ori, image_gray = convert_to_gray(img_path)
 
-plt.subplot(1,2,2)
-plt.title("Sobel")
-plt.imshow(sobel_img)
+    if method.lower() == "sobel":
+        result = sobel_method(image_gray)
+    elif method.lower() == "laplacian":
+        result = laplacian_method(image_gray)
 
-plt.show()
+    # cv2.imwrite(f"../asset/rocky_{method}.jpeg", result)
+
+    rgb_img = cv2.cvtColor(image_ori, cv2.COLOR_BGR2RGB)
+    processed_img = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+
+    plt.subplot(1,2,1)
+    plt.title("Original")
+    plt.imshow(rgb_img, cmap='gray')
+
+    plt.subplot(1,2,2)
+    plt.title(method.capitalize())
+    plt.imshow(processed_img, cmap='gray')
+
+    plt.savefig(f"../asset/rocky_{method}.jpeg")
+    plt.show()
+
+if __name__ == "__main__":
+    chosen_method = input("Choose an edge detection method [sobel/laplacian]: ")
+    image_path = input("Paste image path: ")
+    main(chosen_method, image_path)
